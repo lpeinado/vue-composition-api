1568664a0490c559665c6045e8707e9056f0794e
Lluís Peinado <lluis.peinado@europcar.com>
2023-08-04 14:16:57 +0200

    main:using reactive instead of ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
+    <div>{{ counterData.title }}</div>
     <div>
       <button class="btn" @click="decreaseCounter">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.counter }}</span>
       <button class="btn" @click="increaseCounter">+</button>
     </div>
+    <input type="text" v-model="counterData.title" />
   </div>
 </template>
 <script setup>
-  import { ref } from 'vue';
-  const counter = ref(2);
-  const increaseCounter = () => counter.value++;
-  const decreaseCounter = () => counter.value--;
+  import { reactive } from 'vue';
+  const counterData = reactive({
+    counter: 0,
+    title: 'Home Default Title',
+  });
+  const increaseCounter = () => counterData.counter++;
+  const decreaseCounter = () => counterData.counter--;
+
 </script>
 
 <style>
