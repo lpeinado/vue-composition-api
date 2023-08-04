a02d98be91ca42574ad1c8abee955ad22f24be30
Lluís Peinado <lluis.peinado@europcar.com>
2023-08-04 14:00:07 +0200

    main: first Home component with script setup syntax

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <div>
+      <button class="btn" @click="decreaseCounter">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increaseCounter">+</button>
+    </div>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  const counter = ref(2);
+  const increaseCounter = () => counter.value++;
+  const decreaseCounter = () => counter.value--;
+</script>
+
+<style>
+.home{
+  text-align: center;
+  padding:20px;
+  .btn, .counter {
+     font-size: 40px;
+    margin: 10px;
+  }
+}
+</style>
