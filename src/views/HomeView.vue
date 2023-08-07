3a7433344045f96152b54e2c32e8bfab2617a627
Lluís Peinado <lluis.peinado@europcar.com>
2023-08-07 10:39:39 +0200

    method with event object

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,22 +3,29 @@
   <div class="home">
     <div>{{ counterData.title }}</div>
     <div>
-      <button class="btn" @click="decreaseCounter">-</button>
+      <button class="btn" @click="decreaseCounter(2, $event)">-</button>
       <span class="counter">{{ counterData.counter }}</span>
-      <button class="btn" @click="increaseCounter">+</button>
+      <button class="btn" @click="increaseCounter(3, $event)">+</button>
     </div>
     <input type="text" v-model="counterData.title" />
+    <pre>{{ eventObj }}</pre>
   </div>
 </template>
 <script setup>
-  import { reactive } from 'vue';
+  import { ref, reactive } from 'vue';
   const appTitle = 'My amazing couter app';
   const counterData = reactive({
     counter: 0,
     title: 'Home Default Title',
   });
-  const increaseCounter = () => counterData.counter++;
-  const decreaseCounter = () => counterData.counter--;
+  const eventObj = ref({});
+
+  const increaseCounter = (amount, e) => {
+    counterData.counter += amount;
+    console.log(e);
+    eventObj.value = JSON.parse(JSON.stringify(e));
+  }
+  const decreaseCounter = amount => counterData.counter -= amount;
 
 </script>
 
