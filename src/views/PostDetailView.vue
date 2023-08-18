47cf38fbaf580a85175cf5a82d0589ecef4fef25
Lluís Peinado <lluis.peinado@europcar.com>
2023-08-18 10:01:24 +0200

    main: using route and router composables.

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        A single post detail page
+    </div>
+    <p>This is the detail page for post with id <span style="color:red; font-weight:bold;">{{ route.params.id }}</span></p>
+    <button @click="showPostId">GET ID</button>
+    <div>
+        <router-link to="/posts">&lt; Back to posts</router-link>
+    </div>
+    <div>
+        <button @click="goHomeIn3">Go home in 3 seconds</button>
+    </div>
+    <div>
+        <button @click="goFirstPost">Go to first post</button>
+    </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const showPostId = () => {
+    console.log("ROUTE OBJECT", route.fullPath);
+    console.log("ID", route.params.id);
+};
+
+const goHomeIn3 = () => {
+    setTimeout(() => {
+        router.push({ name : 'home' });
+    }, 1000);
+};
+
+const goFirstPost = () => {
+    router.push({ name : 'postDetail', params : { id : 1 } });
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        Posts view page
+        <ul>
+            <li>
+                <router-link to="/postDetail/1"> Post 1</router-link>
+            </li>
+            <li>
+                <router-link to="/postDetail/2"> Post 2</router-link>
+            </li>
+            <li>
+                <router-link to="/postDetail/3"> Post 3</router-link>
+            </li>
+            <li>
+                <router-link to="/postDetail/4"> Post 4</router-link>
+            </li>
+            <li>
+                <router-link to="/postDetail/5"> Post 5</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
