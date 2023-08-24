8e8dec6fb15862979c1da4e09d4a0c31c4edde4f
Lluís Peinado <lluis.peinado@europcar.com>
2023-08-24 15:52:41 +0200

    main: adding slots and programmatic access to them

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,8 +2,10 @@
     <div>
         <teleport to='body' >
             <div class="modal">
-                <h1>This is a modal</h1>
-                <div>Lorem ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
+                <h1>
+                    <slot name="title"/>
+                </h1>
+                <slot />
                 <button @click="showModal = false">Hide modal</button>
             </div>
         </teleport>
@@ -11,6 +13,8 @@
 </template>
 
 <script setup>
+import { useSlots } from 'vue';
+const slots = useSlots();
 
 </script>
 
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,7 +2,13 @@
     <div>
         <h1>Modals here</h1>
         <button @click="showModal = true">Show modal</button>
-        <Modal v-if="showModal"></Modal>
+        <Modal v-if="showModal">
+            <template #title>
+                <h3>Modal title</h3>
+            </template>
+
+            <div>Lorem ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
+        </Modal>
     </div>
 </template>
 
